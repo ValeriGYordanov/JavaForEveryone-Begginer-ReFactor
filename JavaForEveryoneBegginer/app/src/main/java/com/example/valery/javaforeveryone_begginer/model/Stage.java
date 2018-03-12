30832d2ac52ae7b4e2feeb36b5c4e1386101bcbe
ValeryYordanov <valeri.yordanov@primeholding.com>
2018-03-12 14:11:08 +0200

    Adding buttons forgotten in previous commit, Stage Fragment prepared with testing text.
    Signed-off-by: ValeryYordanov <valeri.yordanov@primeholding.com>

---
+++ JavaForEveryoneBegginer/app/src/main/java/com/example/valery/javaforeveryone_begginer/model/Stage.java
@@ -0,0 +1,12 @@
+package com.example.valery.javaforeveryone_begginer.model;
+
+/**
+ * Created by Valery on 3/12/2018.
+ */
+
+public class Stage {
+
+    public int stageID;
+    private String title;
+    private String text;
+}
