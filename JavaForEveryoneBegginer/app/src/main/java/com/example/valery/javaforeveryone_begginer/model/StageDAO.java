c946cce552a47814bac8b7d4552c18d4248381b6
ValeryYordanov <valeri.yordanov@primeholding.com>
2018-03-19 12:16:43 +0200

    StageDAO fix

--- JavaForEveryoneBegginer/app/src/main/java/com/example/valery/javaforeveryone_begginer/model/StageDAO.java
+++ JavaForEveryoneBegginer/app/src/main/java/com/example/valery/javaforeveryone_begginer/model/StageDAO.java
@@ -6,6 +6,8 @@ import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
+import com.example.valery.javaforeveryone_begginer.model.Stage;
+
 import java.util.List;
 
 import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
